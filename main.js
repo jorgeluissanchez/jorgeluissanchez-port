ca3ae68e972713400dffe381afa0f8768739f99d
Jorge Sanchez <jorgeluissanchezbarreneche@gmail.com>
2022-06-11 22:45:48 -0500

    new features

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var elemsC = document.querySelectorAll('.carousel');
-    var instancesC = M.Carousel.init(elemsC, {
-        padding: 10,
-        noWrap: false,
+$(document).ready(function() {
+
+    $('.carousel').carousel({
+        interval: 2000,
+        wrap: true
+    })
+    $('.carousel').on('touchstart', function(e) {
+        const C = e.originalEvent.touches[0].pageX;
+        $(this).one('touchmove', function(e) {
+            const M = e.originalEvent.touches[0].pageX;
+            const S = 5;
+
+            if (Math.floor(C - M) > S) {
+                $(this).carousel('next');
+            } else if (Math.floor(C - M) < -S) {
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on('touchend', function() {
+            $(this).off('touchmove');
+        });
     });
 });
